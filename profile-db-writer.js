1c932c91c5a0c29d46c603e7b90c497c8457f17a
Terrence Benade <terrencebenade@gmail.com>
2014-11-25 21:30:35 +1100

    added dynamo labs document sdk

--- profile-db-writer.js
+++ profile-db-writer.js
@@ -1,10 +1,13 @@
 var request = require('request')
   , FeedParser = require('feedparser'),
   AWS = require('aws-sdk'),
-  uuid = require('node-uuid');
+  uuid = require('node-uuid'),
+  DOC = require("dynamodb-doc");
 
 AWS.config.region = 'ap-southeast-2';
 var db = new AWS.DynamoDB();
+docClient = new DOC.DynamoDB(db);
+
 var tableParams = {
   TableName: 'profile',
   ProvisionedThroughput: {
@@ -20,13 +23,18 @@ var tableParams = {
 }
 
 function addProfile(profile, callback){
-  var itemData = {"id":{"S":profile.id}, "document":{"M":JSON.stringify({dog:"cat"})}}
+  var itemData = {id:profile.id, name:"Terrence Benade",phone:232323, address:"21 hell drive", kids:{kidone:"Emily", kidtwo:"Hannah"}};
   var itemParams = {TableName:"profile",Item:itemData};
-  db.putItem(itemParams, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
-};
+  docClient.putItem(itemParams, pfunc);
+}
+
+var pfunc = function(err, data) { 
+    if (err) {
+        console.log(err, err.stack);
+    } else {
+        console.log(data);
+    }
+}
 
 addProfile({id:uuid.v1()},function(){
   console.log("All done!!");
